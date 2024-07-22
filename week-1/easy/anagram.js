e0fa3ecf4b51b59febe4f1c36ee04dc2cf7dae54
Anshu kumar <anshu4125kumar@gmail.com>
2024-07-22 14:10:35 +0530

    week-01 easy question anagram Done.

---
+++ week-1/easy/anagram.js
@@ -0,0 +1,27 @@
+
+function isAnagram(str1, str2) {
+
+    const text1=str1.toUpperCase();
+    const text2=str2.toUpperCase();
+    let sum1=0;
+    let sum2=0;
+  if(str1.length!=str2.length){
+    return false;
+  }
+    for(let i=0;i<str1.length;i++)
+    {
+      sum1=text1.charCodeAt(i)+sum1;
+      sum2=text2.charCodeAt(i)+sum2;
+    }
+    if(sum1-sum2==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+  }
+  const result = isAnagram("anshu","nshuu");
+  console.log(result);
