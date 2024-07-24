a980a622585ded5329a6951758d1a781bc5d7b74
Anshu kumar <anshu4125kumar@gmail.com>
2024-07-24 11:39:09 +0530

    todo-list done

--- week-1/hard/todo-list.js
+++ week-1/hard/todo-list.js
@@ -9,7 +9,36 @@
 
   Once you've implemented the logic, test your code by running
 */
-
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
+  add(todo) {
+    this.todos.push(todo)
+  }
+  remove(indexOfTodo) {
+    if (indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1)
+    }
+  }
 
+  update(index, updatedTodo) {
+    if (index < this.todos.length) {
+      this.todos[index] = updatedTodo
+    }
+  }
+  getAll() {
+    return this.todos
+  }
+  get(indexOfTodo) {
+    if (indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo]
+    }
+    else {
+      return null
+    }
+  }
+  clear() {
+    this.todos = []
+  }
 }
